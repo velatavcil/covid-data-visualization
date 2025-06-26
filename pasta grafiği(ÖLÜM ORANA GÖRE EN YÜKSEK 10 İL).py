d4e87370d9b882d59b0a0133253d123a7b8c14f2
Velat Avcil <avcilvelat@gmail.com>
2025-06-26 21:00:56 +0300

    İlk yükleme: COVID veri analizi

--- pasta grafiği(ÖLÜM ORANA GÖRE EN YÜKSEK 10 İL).py
+++ pasta grafiği(ÖLÜM ORANA GÖRE EN YÜKSEK 10 İL).py
@@ -30,3 +30,4 @@ plt.title("Ölüm Oranı En Yüksek 10 İl", fontsize=14)
 plt.tight_layout()
 plt.show()
 (.venv) C:\Users\wavci\PycharmProjects\PythonProject\covid_proje
+git remote add origin https://github.com/velatavcil/covid-data-visualization.git
